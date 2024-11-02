2cc3d878bad75fef81116e85feca90af764ddcc2
Leo Cheung <lappangcheung@gmail.com>
2024-11-01 23:46:43 -0400

    Finished up styling the startup cards

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,10 +2,20 @@ import { formatDate } from "@/lib/utils";
 import {EyeIcon} from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 const StartupCard = ({ post } : { post: StartupTypeCard }) => {
 
-  const { _createdAt, views, author: { _id: authorId , name}, title, category, _id, image } = post;
+  const {
+    _createdAt,
+    views,
+    author: { _id: authorId , name},
+    title,
+    category,
+    _id,
+    image,
+    description
+  } = post;
 
   return (
     <li className="startup-card group">
@@ -40,6 +50,26 @@ const StartupCard = ({ post } : { post: StartupTypeCard }) => {
                  height={48}
                  className="rounded-full" />
         </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">
+          {description}
+        </p>
+        <img src={image}
+             alt="placeholder"
+             className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">
+            {category}
+          </p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>
+            Details
+          </Link>
+        </Button>
       </div>
     </li>
   )
