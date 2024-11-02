09835b2a924fb04b1c08990b97611f91d2d7c373
Leo Cheung <lappangcheung@gmail.com>
2024-11-01 23:42:21 -0400

    Setting up the start cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
 import SearchForm from "@/components/SeachForm";
+import StartupCard from "@/components/StartupCard";
 
 const Home = async({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) => {
 
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: {
+      _id: 1,
+      name: 'John Doe'
+    },
+    _id: 1,
+    description: 'This is a description.',
+    image: 'https://images.pexels.com/photos/2085832/pexels-photo-2085832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    category: 'Robots',
+    title: 'We Robots'
+  }]
 
   return (
     <>
@@ -16,6 +31,24 @@ const Home = async({ searchParams }: {
           Submit Idea,s Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
         <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          { query ? `Search Results for "${query}"` : "All Startups" }
+        </p>
+        <ul className="mt-7 card_grid">
+          {
+            posts?.length > 0 ? (
+              posts.map((post: StartupCardType) => (
+                <StartupCard key={post?._id}
+                             post={post}/>
+              ))
+            ) : (
+              <p className="text-20-semibold text-center">
+                No startups found.
+              </p>
+            )}
+        </ul>
       </section>
     </>
   )
